4abf9cd6d403164a1bca614299ee3f7733fbfd53
Sowhum <sohamnaithani@icloud.com>
2024-07-09 00:28:47 +0530

    data logging + some other stuff
    finished LED, buzzer code, data logging and sensor communication

--- Embedded_temp/src/helpers.h
+++ Embedded_temp/src/helpers.h
@@ -1,18 +1,66 @@
+#ifndef HELPERS_H
+#define HELPERS_H
+
 ////    Includes    ////
 #include <Arduino.h>
-#include "Wire.h"
+#include <Wire.h>
+#include <SD.h>
+#include <SPI.h>
 #include <MPU6050.h>
 #include <KY015.h>
 #include <LiquidCrystal_I2C.h>
 
 ////    Defines     ////
 #define LCD_ADDR      0x27
-#define KY_PIN        5
-#define BUZZER        PIN_A12
-#define LED           PIN_A11 
+#define KY_PIN        4
+#define CS            5
+#define BUZZER        9
+#define LED1          10
+#define LED2          11
+#define WIRE          Wire
+#define MAX_VALUE     32.5
+#define LOGFREQ       5000
 #define SERIAL_MONITOR_BAUD 115200
 
-String file_name = "log.txt";
+////    Objects/Variables     ////
+TStruct t;
+char data[256];
+String fileName = "log.txt";
+unsigned long currentTime;
+unsigned long lastLog=0;
+const char outputFormat[] =
+    R"""(Temp1: %f, Temp2: %f, Humidity: %f, Avg: %f, Time Stamp: %lu
+)""";
+
 MPU mpu;
 KY015 ky = KY015(KY_PIN);
 LiquidCrystal_I2C lcd = LiquidCrystal_I2C(LCD_ADDR, 16, 2);
+
+////    Fucntions   ////
+void checkData(float tKY, float tMPU){
+    if(!ky.get_checksum()){
+        digitalWrite(LED1,HIGH);
+        delay(1000);
+        digitalWrite(LED1,LOW);
+    }
+    if(mpu.readFail==true){
+        digitalWrite(LED2,HIGH);
+        delay(1000);    
+        digitalWrite(LED2,LOW);
+    }
+
+    if(tKY >= MAX_VALUE | tMPU >=MAX_VALUE){
+        digitalWrite(LED1,HIGH);
+        digitalWrite(LED2,HIGH);
+        digitalWrite(BUZZER,HIGH);
+
+        delay(1000);
+        digitalWrite(BUZZER,LOW);
+    }
+    else{
+        digitalWrite(LED1,LOW);
+        digitalWrite(LED2,LOW);
+        digitalWrite(BUZZER,LOW);
+    }
+}
+#endif HELPERS_H
--- Embedded_temp/src/main.cpp
+++ Embedded_temp/src/main.cpp
@@ -1,9 +1,42 @@
 #include "helpers.h"
 
 void setup(){
-    
+  WIRE.begin();
+  Serial.begin(115200);
+  SD.begin(CS);
+  mpu.readFail=false;
+  mpu.pwr_setup();
+
+  delay(1000);    
 }
 
 void loop(){
+  //temp, humidity from KY015
+  float temperatureKY = ky.get_temp();
+  float humidityKY = ky.get_humidity();
 
+  //temp from MPU6050
+  float temperatureMPU = t.TempC;
+
+  checkData(temperatureKY,temperatureMPU);
+  
+  //logging
+  currentTime = millis();
+  sprintf(
+        data, outputFormat,
+        temperatureKY, temperatureMPU, humidityKY,(temperatureKY+temperatureMPU)/2  ,currentTime);
+  if (currentTime - lastLog >= LOGFREQ) {  
+    logData(data);
+    lastLog = currentTime;
+  }
+}
+
+void logData(char data[]) {
+  File logFile = SD.open(fileName, FILE_WRITE);
+  if (logFile) {
+    logFile.println(data);
+    logFile.close();
+  } else {
+    Serial.println("Error opening log.txt");
+  }
 }
